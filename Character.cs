00ea1593663f31e9a6481eb66f7a48eca846bd1c
GamsDataTaboo <104941981+GamsDataTaboo@users.noreply.github.com>
2022-07-20 21:35:26 -0500

    Add files via upload

--- Character.cs
+++ Character.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] Vector3 moveSpeed;
-    [SerializeField] Vector3 moveSpeedMod;
+    [SerializeField] Vector3 _acceleration;
 
-    [SerializeField] Vector3 velocity;
+    [SerializeField] Vector3 _velocity;
+    [SerializeField] Vector3 _velocityMultiplier = Vector3.one;
 
 
     public void Move()
     {
-        transform.Translate(velocity);
+        transform.Translate(Vector3.Scale(_velocity, _velocityMultiplier) * Time.deltaTime);
     }
 }
