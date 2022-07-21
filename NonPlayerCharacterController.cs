7bf8e06b0288253181d9785badba75eea0b51446
GamsDataTaboo <104941981+GamsDataTaboo@users.noreply.github.com>
2022-07-21 03:52:33 -0500

    Add files via upload
    NPCs can now navigate to a target.

--- NonPlayerCharacterController.cs
+++ NonPlayerCharacterController.cs
@@ -1,7 +1,49 @@
+using UnityEngine;
+
+
 public class NonPlayerCharacterController : Character
 {
+    enum States { idle, moveToLocation }
+    [SerializeField] States _state = States.idle;
+    string _stateFunction = nameof(MoveToLocation);
+
+    [SerializeField] Transform _targetLocation;
+    float trueVelocityDistanceBufferMultiplier = 0.1f;
+
+
+    void Start()
+    {
+        switch (_state)
+        {
+            case States.idle:
+                _stateFunction = nameof(Wait);
+                break;
+
+            case States.moveToLocation:
+                TransitionToMoveToLocation();
+                break;
+        }
+    }
     void FixedUpdate()
     {
-        Move();
+        Invoke(_stateFunction, 0f);
+    }
+
+
+    void Wait()
+    {
+    }
+    
+
+    void TransitionToMoveToLocation()
+    {
+        if(_targetLocation != null) _stateFunction = nameof(MoveToLocation);
+    }
+    void MoveToLocation()
+    {
+        transform.right = _targetLocation.position - transform.position;
+
+        if(Vector3.Distance(_targetLocation.position, transform.position) > 
+            (_velocity.x * _velocityMultiplier.x) * trueVelocityDistanceBufferMultiplier) Move();
     }
 }
