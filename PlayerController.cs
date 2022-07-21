5221490363555250f1ab6019fd905fe6e8df29b1
GamsDataTaboo <104941981+GamsDataTaboo@users.noreply.github.com>
2022-07-21 05:11:28 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+public class PlayerController : Character
+{
+    Controls _controls;
+    Vector2 playerControllerAccelerationMultiplier;
+
+
+    void Awake()
+    {
+        _controls = new Controls();
+
+        _controls.Player.Movement.performed 
+            += ctx => playerControllerAccelerationMultiplier = ctx.ReadValue<Vector2>();
+        _controls.Player.Movement.canceled 
+            += ctx => playerControllerAccelerationMultiplier = Vector2.zero;
+    }
+    void OnEnable()
+    {
+        _controls.Enable();
+    }
+    void OnDisable()
+    {
+        _controls.Disable();
+    }
+    public void FixedUpdate()
+    {
+        if (playerControllerAccelerationMultiplier != Vector2.zero)
+        {
+            Accelerate(playerControllerAccelerationMultiplier);
+        }
+        else
+        {
+            SlowDown();
+        }
+        Move();
+    }
+
+
+    public void Accelerate(Vector3 playerControllerAccelerationMultiplier)
+    {
+        _velocity += 
+            Vector3.Scale(_acceleration, playerControllerAccelerationMultiplier) * Time.deltaTime;
+        if (_velocity.magnitude > _maxVelocity.magnitude) _velocity = _maxVelocity;
+    }
+}
